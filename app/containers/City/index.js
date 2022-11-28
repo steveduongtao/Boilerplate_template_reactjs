988cf6df9c34674ae422b4e70966982ed9dc7a85
taodt <duongthetao1011995@gmail.com>
2022-11-28 20:30:54 +0700

    update

--- app/containers/City/index.js
+++ app/containers/City/index.js
@@ -19,7 +19,23 @@ import reducer from './reducer';
 import saga from './saga';
 import makeSelectCity from './selectors';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'none',
+    '&.active > div': {
+      backgroundColor: theme.palette.action.selected,
+    },
+  },
+}));
+
 export function City(props) {
+  const classes = useStyles();
   useInjectReducer({ key: 'city', reducer });
   useInjectSaga({ key: 'city', saga });
   const { city, onGetData, onChangeCloseSnackBar } = props;
@@ -33,48 +49,50 @@ export function City(props) {
   console.log('localData', studentList);
   return (
     <>
-      <TableContainer>
-        <Table aria-label="simple table" size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell width="240px">ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Gender</TableCell>
-              <TableCell>Mark</TableCell>
-              <TableCell>City</TableCell>
-              <TableCell>Ranking</TableCell>
-              <TableCell align="right">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {studentList.map(student => (
-              <TableRow key={student.id}>
-                <TableCell>{student.id}</TableCell>
-                <TableCell>{student.name}</TableCell> <TableCell />
-                <TableCell>
-                  <Box fontWeight="bold">{student.mark}</Box>
-                </TableCell>
-                <TableCell />
-                <TableCell />
-                <TableCell align="right">
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                  <Button
-                    size="small"
-                    color="secondary"
-                    onClick={() => {
-                      // handleRemoveClick(student);
-                    }}
-                  >
-                    Remove
-                  </Button>
-                </TableCell>
+      <Box className={classes.root}>
+        <TableContainer>
+          <Table aria-label="simple table" size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell width="240px">ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Mark</TableCell>
+                <TableCell>City</TableCell>
+                <TableCell>Ranking</TableCell>
+                <TableCell align="right">Actions</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {studentList.map(student => (
+                <TableRow key={student.id}>
+                  <TableCell>{student.id}</TableCell>
+                  <TableCell>{student.name}</TableCell> <TableCell />
+                  <TableCell>
+                    <Box fontWeight="bold">{student.mark}</Box>
+                  </TableCell>
+                  <TableCell />
+                  <TableCell />
+                  <TableCell align="right">
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                    <Button
+                      size="small"
+                      color="secondary"
+                      onClick={() => {
+                        // handleRemoveClick(student);
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
       <CustomSnackbar
         open={changeSnackBar.open}
         variant={changeSnackBar.variant}
